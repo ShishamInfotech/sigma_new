1b0bea42ea81fc401d140e9d5f22168debc11568
Aniket Desai <aniket.d@siltech.co.in>
2025-06-08 00:57:19 +0530

    Matrices issue solved

--- lib/math_view/math_text.dart
+++ lib/math_view/math_text.dart
@@ -12,17 +12,36 @@ class MathText extends StatefulWidget {
 }
 
 class _MathTextState extends State<MathText> {
+  String sanitizeMathExpression(String input) {
 
-  //double _height =10;
+    return input
+        .replaceAll(r'\left[\begin{matrix}', r'$$ \begin{bmatrix}')
+        .replaceAll(r'\end{matrix}\right]', r'\end{bmatrix} $$')
+        .replaceAll(r'\end{matrix} \right]', r'\end{bmatrix} $$')
+        .replaceAll(r'\end{matrix}  \right]', r'\end{bmatrix} $$')
+        .replaceAll(r'\end{matrix} \begin{matrix}', r'\end{matrix}, \quad \begin{matrix}')
+        .replaceAll(r'\(', r'$')                      // Replace \( with $
+        .replaceAll(r'\)', r'$')
+        .replaceAll(r'}$,', r'},')      // Replace \) with $
+        .replaceAll(r' $\ ', r' \quad ')
+        .replaceAll(r'=$\begin{bmatrix}', r'= \begin{bmatrix}')
+        .replaceAll(r'$A', r'$$ A')
+        .replaceAll(r'$B', r'$$ B')
+        .replaceAll(r'\\\', r'\\');
+  }
 
   @override
   Widget build(BuildContext context) {
+    final sanitizedExpression = sanitizeMathExpression(widget.expression);
     return Container(
       height: widget.height,
       child: AndroidView(
         viewType: 'mathview-native',
         layoutDirection: TextDirection.ltr,
-        creationParams: {'expression': widget.expression, 'textSize': 24.0,},
+        creationParams: {
+          'expression': sanitizedExpression,
+          'textSize': 24.0,
+        },
         creationParamsCodec: const StandardMessageCodec(),
       ),
     );
